49ac2cfab5134be4043f510bc769c72b3dcb5814
Yuvraj <yuvivishu17@gmail.com>
2026-04-12 18:12:33 +0530

    Added Advance CSS

--- JS/DOM/day52/task2/script.js
+++ JS/DOM/day52/task2/script.js
@@ -1,13 +1,22 @@
-const para = document.querySelector('p')
-const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-const text = para.innerText
+const para = document.querySelector("p");
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+const text = para.innerText;
 
-para.addEventListener('mouseenter' , ()=>{
+let iteration = 0;
 
-    setInterval(() =>{
-        const str = text.split('').map((char , index) =>{
-            return characters.split("")[Math.floor(Math.random() * 53)]
-        }).join("")
-        para.innerText=str
-    } , 30)
-})
+function randomText() {
+  const str = text
+    .split("")
+    .map((char, index) => {
+      if (index < iteration) {
+        return char;
+      }
+      return characters.split("")[Math.floor(Math.random() * 53)];
+    })
+    .join("");
+  para.innerText = str;
+  iteration += 0.2;
+}
+para.addEventListener("mouseenter", () => {
+  setInterval(randomText, 50);
+});
